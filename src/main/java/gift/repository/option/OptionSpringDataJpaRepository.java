58ee0cd1a3b236010cad975751d0b75fc0578a31
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-20 00:01:04 +0900

    feat: Option레포지토리 수정

--- src/main/java/gift/repository/option/OptionSpringDataJpaRepository.java
+++ src/main/java/gift/repository/option/OptionSpringDataJpaRepository.java
@@ -3,7 +3,6 @@ package gift.repository.option;
 import gift.domain.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +12,4 @@ public interface OptionSpringDataJpaRepository extends JpaRepository<Option, Lon
     Optional<Option> findOptionByName(String name);
 
     Long countOptionByProductId(Long ProductId);
-=======
-public interface OptionSpringDataJpaRepository extends JpaRepository<Option, Long> {
->>>>>>> a833893 (feat: Product 생성자 수정)
 }
