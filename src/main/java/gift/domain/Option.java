ea9c9e13e1bdf8a503a20b60674b92d9ecb7d130
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-20 00:00:25 +0900

    feat: OptionService 작성

--- src/main/java/gift/domain/Option.java
+++ src/main/java/gift/domain/Option.java
@@ -60,4 +60,16 @@ public class Option {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
